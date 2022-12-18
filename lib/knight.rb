51bb475d6cd36d4f3c08054d1b3220829f1d84e8
katie <katiepb525@gmail.com>
2022-12-18 16:13:20 -0600

    create array of possible directions knight can move

--- lib/knight.rb
+++ lib/knight.rb
@@ -10,6 +10,8 @@ class Knight
 # create a list of possible moves using a graph
 
   def get_possible_moves
+    # list of possible directions: up two left one, up two right one, down two left one, down two right one
+    possible_directions = [[1, 2], [-1, 2], [1, -2], [-1, -2]]
 
     # a list of possible moves going down
 
