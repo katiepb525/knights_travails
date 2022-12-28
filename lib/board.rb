4e9df9590aea9519d03a804946d759bd886665f5
katie <katiepb525@gmail.com>
2022-12-28 11:08:03 -0600

    move legal moves method to board class

--- lib/board.rb
+++ lib/board.rb
@@ -3,9 +3,39 @@
 require 'pry-byebug'
 
 class Place
-  def initialize(x, y)
+  def initialize(x, y, board_size)
     @x = x
     @y = y
+    # store possible legal moves for current node
+    @legal_moves = get_legal_moves(@x, @y, board_size)
+  end
+
+  # given starting square as parameter
+  def get_legal_moves(x, y, board_size)
+    # list of possible directions (added to x and y)
+    possible_directions = { l1_u2: [-1, 2], r1_u2: [1, 2], l1_d2: [-1, -2], r1_d2: [1, -2],
+                            l2_u1: [-2, 1], l2_d1: [-2, -1], r2_u1: [2, 1], r2_d1: [2, -1] }
+
+    # create adjacency matrix to store all possible moves **from given square**
+    # use dimensions of board to determine size of matrix
+    # 0 is default
+    matrix = Array.new(board_size) { Array.new(board_size, 0) }
+
+    # for every key in possible directions
+    possible_directions.each do |k, _v|
+      # # get sum of x + y values
+      result = []
+      result.push((x + possible_directions[k][0]) )
+      result.push((y + possible_directions[k][1]) )
+
+      # catch instances of x or y values being above (or equal to) eight
+      # catch instances of x or y values being less than zero
+      next if result[0] >= 8 || result[1] >= 8 || result[0].negative? || result[1].negative?
+
+      # mark edge using result's axis values
+      matrix[(result[0])][(result[1])] = 1
+    end
+    matrix
   end
 end
 
--- lib/knight.rb
+++ lib/knight.rb
@@ -4,40 +4,13 @@ require_relative 'board'
 require 'pry-byebug'
 
 class Knight
-  def initialize
-
-  end
+  def initialize; end
 
   # create a list of possible moves **from a given square**
 
-  # given starting square as parameter
-  def get_possible_moves(place, board)
-    # list of possible directions (added to x and y)
-    possible_directions = { l1_u2: [-1, 2], r1_u2: [1, 2], l1_d2: [-1, -2], r1_d2: [1, -2],
-                            l2_u1: [-2, 1], l2_d1: [-2, -1], r2_u1: [2, 1], r2_d1: [2, -1] }
-
-    # create adjacency matrix to store all possible moves **from given square**
-    # use dimensions of board to determine size of matrix
-    # 0 is default
-    matrix = Array.new(board.height) { Array.new(board.width, 0) }
-
-    # for every key in possible directions
-    possible_directions.each do |k, _v|
-
-      # # get sum of x + y values
-      result = []
-      result.push((place[:x] + possible_directions[k][0]))
-      result.push((place[:y] + possible_directions[k][1]))
-
-      # catch instances of x or y values being above eight
-      # catch instances of x or y values being less than zero
-      next if result[0] > 8 || result[1] > 8 || result[0].negative? || result[1].negative?
-
-      # mark edge using result's axis values
-      matrix[(result[1])][(result[0])] = 1
-    end
-    matrix
-  end
+  # find the shortest possible way from coordinate start to coordinate end
+  # using breadth first level order
+  def knight_moves(k_start, k_end); end
 end
 
 board = Board.new
@@ -48,10 +21,12 @@ knight = Knight.new
 
 place = { x: 4, y: 3 }
 
-possible_moves = knight.get_possible_moves(place, board)
+possible_moves = knight.get_legal_moves(place, board)
 
 possible_moves.each do |e|
   p e
 end
 
 p possible_moves[2][1]
+
+# knight.knight_moves([3,4],[4,3])
