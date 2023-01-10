3669a858393e1d1e987505fb39dcec057dda8564
katie <katiepb525@gmail.com>
2023-01-09 18:01:25 -0600

    fix bfs

--- knight_moves.rb
+++ knight_moves.rb
@@ -22,7 +22,7 @@ class Knight
     possible_directions = [[2,1],[1,2],[-1,2],[-2,1],[-2,-1],[-1,-2],[1,-2],[2,-1]]
     # initalize array
     list = []
-
+    
     # for every key in possible directions
     possible_directions.each do |k|
 
@@ -35,56 +35,72 @@ class Knight
       # catch instances of x or y values being less than zero (cause there are no values below 0,0)
       next if result[0] >= 8 || result[1] >= 8 || result[0].negative? || result[1].negative?
 
-      p result
-
       # create new place node and push into list
       new_place = Place.new(result[0], result[1])
       list.push(new_place)
 
     end
-
     return list
 
   end
 
-
-
-
   # perform a breadth first search from a given start to a given end coordinate
   def knight_moves(start_coord, end_coord)
 
-    # convert start coord into a node
-    start_coord = Place.new(start_coord, nil, get_legal_moves(start_coord[0], start_coord[1]))
+    # initalize start's distance with 0
+    start_coord.distance = 0;
 
     # create a queue to store the node to be looked at, intialized with start
     queue = [start_coord]
-
-    # make a list of the visited nodes
-    visited_nodes = []
 
     until queue.empty?
       # get node from beginning of queue and store current coord
       current = queue.shift
 
       # if the current node is the end node, stop
-      break if current.position == end_coord
+      if current.x == end_coord.x && current.y == end_coord.y
+        predecessors = []
+        p "first predecessor: #{current.x}, #{current.y}"
+        # push current to predecessors
+        predecessors.push(current);
+
+        # store current parent being looked at
+        curr_parent = current.predecessor
+        # push to predecessors
+        predecessors.push(curr_parent)
+
+        p "next predecessor: #{curr_parent.x}, #{curr_parent.y}"
+
+        # look at next parent
+        next_parent = curr_parent.predecessor
+
+        # loop through predecessors of each move and store in array
+        until (next_parent == start_coord) do
+          p "next predecessor: #{next_parent.x}, #{next_parent.y}"
+          # update current parent
+          predecessors.push(next_parent)
+          # loop to next
+          next_parent = next_parent.predecessor
+        end
+
+        predecessors.push(next_parent)
+        p "last predecessor: #{next_parent.x}, #{next_parent.y}"
+        return
+      end
 
       # look at all legal moves for current node
-      enq_list = current.legal_moves
+      enq_list = legal_moves(current)
 
       # for each legal move
       enq_list.each do |move|
-        # create a new node and set it's origin to current node being looked at
-        new_node = Place.new(move, current)
-
-        # if node hasn't been visited
-        if !(visited_nodes.include?(new_node))
-          # push into end of queue
-          queue.push(new_node)
-          # mark as visited
-          visited_nodes.push(new_node)
+        if (move.distance.nil?)
+          # p "checking [#{move.x}, #{move.y}]"
+          move.distance = current.distance + 1
+          move.predecessor = current
+          queue.push(move)
+          # p "predecessor: [#{move.predecessor.x}, #{move.predecessor.y}]"
+          # p "distance: #{move.distance}"
         end
-        p new_node
       end
     end 
 
@@ -95,4 +111,4 @@ knight = Knight.new
 start = Place.new(3,3)
 place_end = Place.new(4,3)
 
-p knight.legal_moves(start)
+p knight.knight_moves(start, place_end)
